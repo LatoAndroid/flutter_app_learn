e7408c6b0febe4ed6d98a4a082f24b8633405541
jiangwei_android@163.com <jiangwei_android@163.com>
2021-02-26 14:25:38 +0800

    [kyle][dart学习][一个简单的页面ui]

---
+++ lib/demoWidget.dart
@@ -0,0 +1,64 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class DemoWidget extends StatefulWidget {
+  IconData icon;
+
+  DemoWidget(this.icon);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _DemoState(icon);
+  }
+}
+
+class _DemoState extends State<DemoWidget> {
+  IconData icon;
+
+  _DemoState(this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: EdgeInsets.all(10.0),
+      width: 100,
+      height: 100,
+      color: Colors.deepOrange,
+      child: Center(
+        child: Row(
+          //主轴居中
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          textDirection: TextDirection.ltr,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Image(
+                image: AssetImage("assets/images/pay_happy.png"),
+                width: 100,
+                height: 100,
+              ),
+            ),
+
+
+            Padding(padding: EdgeInsets.only(left: 10)),
+
+            Expanded(
+              flex: 1,
+              child:  Text(
+                "我好帅啊啊啊啊啊啊啊啊啊啊啊啊",
+                style: TextStyle(color: Colors.grey, fontSize: 20.0),
+                overflow: TextOverflow.ellipsis,
+                textDirection: TextDirection.ltr,
+                maxLines: 1,
+              )
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_1/demoWidget.dart';
 import 'package:flutter_app_1/testDartOne.dart';
 
 import 'testDartFun.dart';
@@ -12,37 +13,18 @@ void main() {
   //one.publicFun();
   //testFun.mainOut();
   testObj.mainOut();
-  //runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return DemoWidget(Icons.star);
   }
 }
 
-class MyHomePage extends StatefulWidget {
+/*class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -125,4 +107,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+}*/
